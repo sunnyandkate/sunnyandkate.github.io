003db495a7883a40ece441eb21aca9cdae14c551
kate <77579746+sunnyandkate@users.noreply.github.com>
2025-05-05 18:32:09 +0100

    new commit

--- js/cookieconsent-init.js
+++ js/cookieconsent-init.js
@@ -61,7 +61,7 @@ cc.run({
     languages: {
         'en': {
             consent_modal: {
-                title: cookie + ' We use cookies! ',
+                title:  ' We use cookies! ',
                 description: 'Hi, this website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent. <button type="button" data-cc="c-settings" class="cc-link">Let me choose</button>',
                 primary_btn: {
                     text: 'Accept all',
---
+++ js/titleScreen.js
@@ -0,0 +1,1 @@
+var startBtn = document.querySelector(".startBtn");
